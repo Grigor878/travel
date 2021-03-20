5ab6dc4a6692e2eae6ad2a571e8defee114eb4a0
Grigor878 <81014376+Grigor878@users.noreply.github.com>
2021-03-20 14:05:17 +0400

    Add files via upload

---
+++ +Travel In Armenia (Landing Responsive Site)/js/script.js
@@ -0,0 +1,48 @@
+//isShowBtn & isShowBtn_hide
+window.onload = () => {
+
+    const scrollBtn = document.querySelector('.isShowBtn')
+
+    window.onscroll = () => {
+        if (window.scrollY > 700) {
+            scrollBtn.classList.remove('isShowBtn_hide');
+        } else if (window.scrollY < 700) {
+            scrollBtn.classList.add('isShowBtn_hide');
+        }
+    };
+
+    scrollBtn.onclick = () => {
+        window.scrollTo(0, 0);
+    };
+};
+
+
+//Section-Gallery
+//var slideIndex = 1;
+//showSlides(slideIndex);
+//
+//function plusSlides(n) {
+//  showSlides(slideIndex += n);
+//}
+//
+//function currentSlide(n) {
+//  showSlides(slideIndex = n);
+//}
+//
+//function showSlides(n) {
+//  var i;
+//  var slides = document.getElementsByClassName("mySlides");
+//  var dots = document.getElementsByClassName("demo");
+//  var captionText = document.getElementById("caption");
+//  if (n > slides.length) {slideIndex = 1}
+//  if (n < 1) {slideIndex = slides.length}
+//  for (i = 0; i < slides.length; i++) {
+//      slides[i].style.display = "none";
+//  }
+//  for (i = 0; i < dots.length; i++) {
+//      dots[i].className = dots[i].className.replace(" active", "");
+//  }
+//  slides[slideIndex-1].style.display = "block";
+//  dots[slideIndex-1].className += " active";
+//  captionText.innerHTML = dots[slideIndex-1].alt;
+//}
